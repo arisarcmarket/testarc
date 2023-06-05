95b8d6a7f826a29aaa8638ef16c4cb6fc12358a1
arisarcmarket <101665656+arisarcmarket@users.noreply.github.com>
2023-06-05 14:14:17 +0700

    Update helloworld.sol

--- contracts/helloworld.sol
+++ contracts/helloworld.sol
@@ -1,2 +1,13 @@
 pragma solidity 0.6.12;
 
+
+contract HelloWorld {
+
+	function get() public pure returns(string memory) {
+		return "Hello Moonbeam";
+	}
+
+	function getParams(string memory myV) returns(string memory) {
+		return myV;
+	}
+}
